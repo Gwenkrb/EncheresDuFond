eaf16e4fc54ce33244604ace855f85773de99f06
“Gwenkrb” <gwen.kerbastard@gmail.com>
2024-07-01 14:09:22 +0200

    first commit

---
+++ src/main/java/fr/eni/encheresdufond/EncheresDuFondApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.encheresdufond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresDuFondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresDuFondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/encheresdufond/EncheresDuFondApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.encheresdufond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresDuFondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
