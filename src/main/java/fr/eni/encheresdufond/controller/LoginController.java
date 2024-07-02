01707e170c934390bdae3c531cc233db643faf5d
“Gwenkrb” <gwen.kerbastard@gmail.com>
2024-07-02 16:27:54 +0200

    début mapping pour inscription

--- src/main/java/fr/eni/encheresdufond/controller/LoginController.java
+++ src/main/java/fr/eni/encheresdufond/controller/LoginController.java
@@ -3,13 +3,16 @@ package fr.eni.encheresdufond.controller;
 import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fr.eni.encheresdufond.bll.UtilisateurService;
 import fr.eni.encheresdufond.bo.Utilisateur;
-import fr.eni.encheresdufond.dal.UtilisateurDAO;
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes({"membreSession"})
@@ -24,6 +27,11 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		return "index";
 	}
 	
 	@GetMapping("/connexion")
@@ -59,9 +67,24 @@ public class LoginController {
 			membreSession.setAdminisatrateur(false);
 		}
 		
-		return "index";
+		return "redirect:/";
 	}
 
+	@GetMapping("/inscription")
+	public String afficherInscriptionUtilisateur(Model model) {
+		
+		Utilisateur u = new Utilisateur();
+		model.addAttribute("utilisateur", u);
+		
+		return "inscription";
+	}
+	
+	@PostMapping("/inscription")
+	public String inscrireUnUtilisateur(@Valid @ModelAttribute("utilisateur") Utilisateur utilisateur, BindingResult bindingResult) {
+		utilisateurService.creerUtilisateur(utilisateur);
+		return "redirect:/connexion";
+	}
+	
 	@ModelAttribute("membreSession")
 	public Utilisateur chargerMembreSession() {
 		return new Utilisateur();
